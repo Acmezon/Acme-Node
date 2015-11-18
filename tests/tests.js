5c2f673b2f5f38a5e122d3fb9b21abe17ec47d02
Ale Sanchez <alesanmed@alum.us.es>
2015-11-18 07:33:43 +0000

    Modificado el archivo de tests para que, antes de cada uno, pueble la base de datos con 3 contactos de ejemplo. Borrando lo que hubiese.

--- tests/tests.js
+++ tests/tests.js
@@ -1,7 +1,44 @@
+var DataStore = require('nedb');
+var path = require('path');
+var fs = require('fs');
+
 exports.config = {
 	seleniumAddress: 'http://localhost:4444/wd/hub',
 	specs: [
 		'e2e/TC01-loadData.js',
+		'e2e/TC02-deleteEntry.js',
 		'backend/TC01-getNotFound.js'
-	]
+	],
+	beforeLaunch: function() {
+		var dbFileName = path.join(__dirname, '../','contacts.json');
+		var db = new DataStore({
+			filename : dbFileName,
+			autoload : true
+		});
+
+		db.remove({}, { multi: true }, function (err, numRemoved) {
+			console.log("Removed: " + numRemoved);
+			person1 = {
+				name : 'Pablo',
+				email : 'pafmon@gmail.com',
+				number : '(34) 954 21 31 34'
+			};
+
+			person2 = {
+				name : 'Pedro',
+				email : 'pedro@gmail.com',
+				number : '(34) 657 21 57 89'
+			};
+
+			person3 = {
+				name : 'Ale',
+				email : 'ale@gmail.com',
+				number : '(34) 654 87 98 78'
+			};
+
+			db.insert([person1, person2, person3], function (err, newDocs) {
+				console.log("Inserted: " + newDocs.length)
+			});
+		});
+	},
 };
