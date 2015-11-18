1d80c95be1eb81307b7caa70cfdc9f80617dadcc
Ale Sanchez <alesanmed@alum.us.es>
2015-11-18 07:34:43 +0000

    Adaptado el servidor para refrescar la base de datos en la peticion GET a contacts y tener en cuenta los datos poblados por los tests

--- server.js
+++ server.js
@@ -15,33 +15,15 @@ var db = new DataStore({
 	autoload : true
 });
 
-
-db.find({}, function (err, contacts) {
-	if( contacts.length == 0 ) {
-		console.log("No contacts, loading initial ones.");
-		person1 = {
-			name : 'Pablo',
-			email : 'pafmon@gmail.com',
-			number : '(34) 954 21 31 34'
-		};
-
-		person2 = {
-			name : 'Pedro',
-			email : 'pedro@gmail.com',
-			number : '(34) 657 21 57 89'
-		};
-
-		db.insert([person1, person2]);
-	} else {
-		console.log(contacts.length + " contacts found.");
-	}
-});
-
 app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/contacts", function(req, res) {
+	db = new DataStore({
+		filename : dbFileName,
+		autoload : true
+	});
 	db.find({}, function (err, contacts) {
 		res.json(contacts);
 	});
