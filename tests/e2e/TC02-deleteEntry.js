47cefee4b0da5c27405f2902ef7950f244393373
alesanmed <alesanchezmedina@gmail.com>
2015-11-18 15:51:30 +0100

    task(GIT:Test e2e: Prueba de borrado) toggl(1200)

--- tests/e2e/TC02-deleteEntry.js
+++ tests/e2e/TC02-deleteEntry.js
@@ -3,16 +3,20 @@ describe('An entry is deleted', function () {
 	it('Should delete an entry', function (){
 		browser.get('http://localhost:5000');
 
-		var contacts = element.all(by.repeater('contact in contactlist'));
+		var contacts_nr = -1;
+		var contacts = element.all(by.repeater('contact in contactlist')).count().then(function(contacts_def) {
+			contacts_nr = contacts_def;
+		});
 
 		element(by.css('.btn-danger')).click();
 		browser.waitForAngular();
 
-		var new_contacts = element.all(by.repeater('contact in contactlist'));
-		contacts.count().then(function(contacts) {
-			contacts_new.count().then(function(new_contacts){
-				expect(new_contacts).toEqual(contacts - 1);
-			});
+		var new_contacts_nr = -1;
+		var new_contacts = element.all(by.repeater('contact in contactlist')).count().then(function(new_contacts_def) {
+			new_contacts_nr = new_contacts_def;
+
+			expect(new_contacts_nr).toEqual(contacts_nr - 1);
 		});
+		
 	});
 });
